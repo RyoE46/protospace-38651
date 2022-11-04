544c67e0e57bf89d2ad8bd5b75be12d7cf00c5e9
RyoE46 <mido88ryo@gmail.com>
2022-11-04 16:32:03 +0900

    render :new
    createアクションに、データが保存されなかったときは新規投稿ページへ戻るようrenderを用いて記述した
     バリデーションによって保存ができず投稿ページへ戻ってきた場合でも、入力済みの項目（画像以外）は消えないことを確認した
     ヘッダーの「New Proto」ボタンから、新規投稿ページに遷移するようにパスを設定した（rails routesを用いて確認する）

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,19 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.all
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
